1252a3fc807f1a394c31125e7cef6c767aa4ff96
evb0110 <7149553@gmail.com>
2019-02-01 21:42:00 +0300

    solved!

--- src/js/sortByProperties.js
+++ src/js/sortByProperties.js
@@ -1,27 +1,36 @@
 const makeUnsortedArray = (obj) => {
   const result = [];
+
   for (const prop in obj) {
     if (obj.hasOwnProperty(prop)) {
       result.push({
         key: prop,
-        value: obj[prop]
+        value: obj[prop],
       });
     }
   }
+
   return result;
-}
+};
 
 const sortByProperties = (initialObj, importantProps) => {
   const unsortedArray = makeUnsortedArray(initialObj);
+
   const importantArray = unsortedArray.filter(obj => importantProps.includes(obj.key));
   // will be sorted by the importance of properties
-  const unimportantArray = unsortedArray.filter(obj => !importantProps.includes(obj.key));
+
+  const unimportantArray = (
+    unsortedArray.filter(obj => !importantProps.includes(obj.key))
+  );
   // will be sorted by the alphabetical value of properties
-  const importantArraySorted = importantArray.sort((a, b) =>
-    importantProps.indexOf(a.key) - importantProps.indexOf(b.key));
-  const unimportantArraySorted = unimportantArray.sort((a, b) =>
-    a.key < b.key ? -1 : 1);
+
+  const importantArraySorted = (
+    importantArray.sort((a, b) => importantProps.indexOf(a.key) - importantProps.indexOf(b.key))
+  );
+
+  const unimportantArraySorted = unimportantArray.sort((a, b) => (a.key < b.key ? -1 : 1));
+
   return [...importantArraySorted, ...unimportantArraySorted];
-}
+};
 
 export default sortByProperties;
--- src/js/sortByProperties.test.js
+++ src/js/sortByProperties.test.js
@@ -1,36 +1,36 @@
 import sortByProperties from './sortByProperties';
 
 test('testing with data from the assignment', () => {
-  const importantProps = ["name", "level"];
+  const importantProps = ['name', 'level'];
 
   const obj = {
     name: 'мечник',
     health: 10,
     level: 2,
     attack: 80,
-    defence: 40
+    defence: 40,
   };
 
   const expected = [{
-      key: "name",
-      value: "мечник"
-    }, // порядок взят из массива с ключами
-    {
-      key: "level",
-      value: 2
-    }, // порядок взят из массива с ключами
-    {
-      key: "attack",
-      value: 80
-    }, // порядок по алфавиту (т.к. в массиве с ключами нет значения "attack")
-    {
-      key: "defence",
-      value: 40
-    }, // порядок по алфавиту (т.к. в массиве с ключами нет значения "defence")
-    {
-      key: "health",
-      value: 10
-    } // порядок по алфавиту (т.к. в массиве с ключами нет значения "health")
+    key: 'name',
+    value: 'мечник',
+  }, // порядок взят из массива с ключами
+  {
+    key: 'level',
+    value: 2,
+  }, // порядок взят из массива с ключами
+  {
+    key: 'attack',
+    value: 80,
+  }, // порядок по алфавиту (т.к. в массиве с ключами нет значения "attack")
+  {
+    key: 'defence',
+    value: 40,
+  }, // порядок по алфавиту (т.к. в массиве с ключами нет значения "defence")
+  {
+    key: 'health',
+    value: 10,
+  }, // порядок по алфавиту (т.к. в массиве с ключами нет значения "health")
   ];
 
   expect(sortByProperties(obj, importantProps)).toEqual(expected);
@@ -38,73 +38,73 @@ test('testing with data from the assignment', () => {
 
 
 test('testing with all props important', () => {
-  const importantProps = ["name", "level", "health", "attack", "defence"];
+  const importantProps = ['name', 'level', 'health', 'attack', 'defence'];
 
   const obj = {
     name: 'мечник',
     health: 10,
     level: 2,
     attack: 80,
-    defence: 40
+    defence: 40,
   };
 
   const expected = [{
-      key: "name",
-      value: "мечник"
-    }, 
-    {
-      key: "level",
-      value: 2
-    }, 
-    {
-      key: "health",
-      value: 10
-    }, 
-    {
-      key: "attack",
-      value: 80
-    }, 
-    {
-      key: "defence",
-      value: 40
-    }, 
+    key: 'name',
+    value: 'мечник',
+  },
+  {
+    key: 'level',
+    value: 2,
+  },
+  {
+    key: 'health',
+    value: 10,
+  },
+  {
+    key: 'attack',
+    value: 80,
+  },
+  {
+    key: 'defence',
+    value: 40,
+  },
   ];
 
   expect(sortByProperties(obj, importantProps)).toEqual(expected);
 });
 
 test('testing with all props unimportant, only lexicographic sort', () => {
-  const importantProps = ["width", "height", "color"];
+  const importantProps = ['width', 'height', 'color'];
 
   const obj = {
     name: 'мечник',
     health: 10,
     level: 2,
     attack: 80,
-    defence: 40
+    defence: 40,
   };
 
   const expected = [
     {
-      key: "attack",
-      value: 80
-    }, 
+      key: 'attack',
+      value: 80,
+    },
     {
-      key: "defence",
-      value: 40
-    }, 
+      key: 'defence',
+      value: 40,
+    },
     {
-      key: "health",
-      value: 10
-    }, 
+      key: 'health',
+      value: 10,
+    },
     {
-      key: "level",
-      value: 2
-    }, 
+      key: 'level',
+      value: 2,
+    },
     {
-      key: "name",
-      value: "мечник"
-    }, 
+      key: 'name',
+      value: 'мечник',
+    },
   ];
 
   expect(sortByProperties(obj, importantProps)).toEqual(expected);
@@ -114,45 +114,43 @@ test('testing for object with some properties from prototype', () => {
 // This test is made to ensure 100%-coverage, since the function under test
 // checks obj.hasOwnProperty()
 
-  const importantProps = ["width", "height", "color"];
+  const importantProps = ['width', 'height', 'color'];
 
   const protoobj = {
     color: 'red',
   };
   const trueobj = {
-    name: "мечник",
+    name: 'мечник',
     health: 10,
     level: 2,
     attack: 80,
-    defence: 40
+    defence: 40,
   };
-  const obj = {...trueobj };
+  const obj = { ...trueobj };
   Object.setPrototypeOf(obj, protoobj);
 
   const expected = [
     {
-      key: "attack",
-      value: 80
-    }, 
+      key: 'attack',
+      value: 80,
+    },
     {
-      key: "defence",
-      value: 40
-    }, 
+      key: 'defence',
+      value: 40,
+    },
     {
-      key: "health",
-      value: 10
-    }, 
+      key: 'health',
+      value: 10,
+    },
     {
-      key: "level",
-      value: 2
-    }, 
+      key: 'level',
+      value: 2,
+    },
     {
-      key: "name",
-      value: "мечник"
-    }, 
+      key: 'name',
+      value: 'мечник',
+    },
   ];
-  
+
   expect(sortByProperties(obj, importantProps)).toEqual(expected);
-
 });
-
