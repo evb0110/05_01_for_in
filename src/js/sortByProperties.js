85b9bbda37e4051440c79d77f3d4205b09d4985b
evb0110 <7149553@gmail.com>
2019-02-01 17:45:56 +0300

    program works, test fails

---
+++ src/js/sortByProperties.js
@@ -0,0 +1,26 @@
+const makeUnsortedArray = (obj) => {
+  const result = [];
+  for (prop in obj) {
+    if (obj.hasOwnProperty(prop)) {
+      result.push({
+        key: prop,
+        value: obj[prop]
+      });
+    }
+  }
+  return result;
+}
+
+const sortByProperties = (initialObj, importantProps) => {
+  const unsortedArray = makeUnsortedArray(initialObj);
+  const importantArray = unsortedArray.filter(obj => importantProps.includes(obj.key));
+  const unimportantArray = unsortedArray.filter(obj => !importantProps.includes(obj.key));
+  const importantArraySorted = importantArray.sort((a, b) =>
+    importantProps.indexOf(a.key) - importantProps.indexOf(b.key));
+  const unimportantArraySorted = unimportantArray.sort((a, b) =>
+    a.key < b.key ? -1 :
+    a.key == b.key ? 0 : 1);
+  return [...importantArraySorted, ...unimportantArraySorted];
+}
+
+export default sortByProperties;
---
+++ src/js/sortByProperties.test.js
@@ -0,0 +1,37 @@
+import sortByProperties from './sortByProperties';
+
+test('testing with data from the assignment', () => {
+  const importantProps = ["name", "level"];
+
+  const obj = {
+    name: 'мечник',
+    health: 10,
+    level: 2,
+    attack: 80,
+    defence: 40
+  };
+
+  const expected = [{
+      key: "name",
+      value: "мечник"
+    }, // порядок взят из массива с ключами
+    {
+      key: "level",
+      value: 2
+    }, // порядок взят из массива с ключами
+    {
+      key: "attack",
+      value: 80
+    }, // порядок по алфавиту (т.к. в массиве с ключами нет значения "attack")
+    {
+      key: "defence",
+      value: 40
+    }, // порядок по алфавиту (т.к. в массиве с ключами нет значения "defence")
+    {
+      key: "health",
+      value: 10
+    } // порядок по алфавиту (т.к. в массиве с ключами нет значения "health")
+  ];
+
+  expect(sortByProperties(obj)).toEqual(expect);
+});
